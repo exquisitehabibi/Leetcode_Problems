71f9281d307ad8c87961d30859e7b014e8974f05
Arun Thakur <120034666+exquisitehabibi@users.noreply.github.com>
2025-01-27 19:53:49 +0530

    Create 69. Sqrt(x).cpp

---
+++ 69. Sqrt(x).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        // long long i;
+        // for( i =0; i < 100000; i++){
+        //     if(i*i>x){
+        //         return i-1;
+        //     }
+        //     if(i*i==x){
+        //         return i;
+        //     }
+        // }
+        // return 0;
+        return sqrt(x);
+        
+    }
+};
