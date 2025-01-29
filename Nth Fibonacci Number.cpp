6602bd1d8d9e8ee081f609a86edc7e8bd3367a73
Arun Thakur <120034666+exquisitehabibi@users.noreply.github.com>
2025-01-29 18:30:47 +0530

    Nth Fibonacci Number.cpp

---
+++ Nth Fibonacci Number.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        /*
+        *  Write your code here. 
+         *  Read input as specified in the question.
+         *  Print output as specified in the question.
+        */
+        int n;
+        cin>>n;
+        int arr[n];
+        arr[0]=1; arr[1]=1;
+        for(int i =2;i<n;i++){
+                arr[i]=arr[i-1]+arr[i-2];
+        }
+        cout<<arr[n-1];
+        return 0;
+}
