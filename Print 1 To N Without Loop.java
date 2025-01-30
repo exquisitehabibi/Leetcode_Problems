afb486a59da39b62b6ab73300f75c73212d01a86
Arun Thakur <120034666+exquisitehabibi@users.noreply.github.com>
2025-01-30 17:49:44 +0530

    Update Print 1 To N Without Loop.java

--- Print 1 To N Without Loop.java
+++ Print 1 To N Without Loop.java
@@ -1,15 +1,24 @@
 class Solution {
-    
-    public void pr(int i, int n){
-        System.out.print(i+" ");
-        if(i==n){ return ;}
-        i++;
-        pr(i,n);
-    }
-    
     public void printNos(int n) {
-        int i =1;
-        pr(i,n);
+        if (n == 0) return; 
+        printNos(n - 1); 
+        cout << n << " ";
         // Your code here
     }
 }
+
+// class Solution {
+    
+//     public void pr(int i, int n){
+//         System.out.print(i+" ");
+//         if(i==n){ return ;}
+//         i++;
+//         pr(i,n);
+//     }
+    
+//     public void printNos(int n) {
+//         int i =1;
+//         pr(i,n);
+//         // Your code here
+//     }
+// }
